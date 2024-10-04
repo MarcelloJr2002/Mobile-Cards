3779f55855b3f37c80bae0689425bc8ddff6b670
MarcelloJr2002 <maciej.chanko@gmail.com>
2024-10-04 13:46:46 +0200

    Photon BlackJack started

--- Mobile_Cards/Mobile_Cards/Assets/Scripts/Managment/BaseGameManager.cs
+++ Mobile_Cards/Mobile_Cards/Assets/Scripts/Managment/BaseGameManager.cs
@@ -1,16 +1,30 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseGameManager : MonoBehaviour
+public class BaseGameManager : MonoBehaviourPunCallbacks
 {
+    public CardDealer dealer;
+    
     public virtual void GameStart()
     {
-
+        dealer.numberOfPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
     }
 
     public virtual void EndGame()
     {
 
+    }
+
+    public virtual void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        dealer.numberOfPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
+    }
+
+    public virtual void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        dealer.numberOfPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
     }
 }
--- Mobile_Cards/Mobile_Cards/Assets/Scripts/Managment/BlackJackGameManager.cs
+++ Mobile_Cards/Mobile_Cards/Assets/Scripts/Managment/BlackJackGameManager.cs
@@ -1,8 +1,27 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BlackJackGameManager : BaseGameManager
 {
+    public CardDealer cardDealer;
+    
+    public void Hit()
+    {
+
+    }
+
+    public void Stand()
+    {
+
+    }
+
+    public void CheckGameConditions()
+    {
+
+    }
+
     
 }
